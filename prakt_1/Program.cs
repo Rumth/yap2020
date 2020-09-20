8f4a1309f794c4f64bb7593c326ffb02e453e190
Rumth <wtfhiujiwtf@gmail.com>
2020-09-20 16:48:25 +0300

    упражнение 2 выполено

--- prakt_1/Program.cs
+++ prakt_1/Program.cs
@@ -3,24 +3,27 @@
 namespace work
 {
     struct Person{
+        public enum Genders: int{Male, Female};
         public string firstName;
         public string lastName;
         public int age;
-        public Person(string _firstName, string _lastName, int _age){
+        public Genders gender;
+        public Person(string _firstName, string _lastName, int _age, Genders _gender){
             firstName = _firstName;
             lastName = _lastName;
             age = _age;
+            gender = _gender;
         }
         public override string ToString()
         {
-            return firstName + " " + lastName + ", age " + age;
+            return firstName + " " + lastName + ", age " + age + ", " + gender;
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Person p = new Person("Alan", "Park", 27);
+            Person p = new Person("Ruth", "Waelry", 25, Person.Genders.Female);
             Console.WriteLine(p);
         }
     }

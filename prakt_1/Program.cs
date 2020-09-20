d0877f0403ae5c7fb636d2d26d84036634fa6411
Rumth <wtfhiujiwtf@gmail.com>
2020-09-20 16:29:57 +0300

    упражнение 1 сделано

--- prakt_1/Program.cs
+++ prakt_1/Program.cs
@@ -2,11 +2,26 @@
 
 namespace work
 {
+    struct Person{
+        public string firstName;
+        public string lastName;
+        public int age;
+        public Person(string _firstName, string _lastName, int _age){
+            firstName = _firstName;
+            lastName = _lastName;
+            age = _age;
+        }
+        public override string ToString()
+        {
+            return firstName + " " + lastName + ", age " + age;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello ASS!");
+            Person p = new Person("Alan", "Park", 27);
+            Console.WriteLine(p);
         }
     }
 }
